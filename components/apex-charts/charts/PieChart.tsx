a56ef25418d626b6e0f55388957608173879ac26
GeonHo Han <hrg921@gmail.com>
2020-03-29 16:29:51 +0900

    feat: remove responsive rules

--- components/apex-charts/charts/PieChart.tsx
+++ components/apex-charts/charts/PieChart.tsx
@@ -3,28 +3,18 @@ import ReactApexChart from 'react-apexcharts';
 
 import { useChartId } from './useChartId';
 
-export const PieChart = () => {
+type Props = {
+  series: number[];
+};
+
+export const PieChart: React.FC<Props> = ({ series }) => {
   const chartId = useChartId();
-  const series = [44, 55, 13, 43, 22];
   const options = {
     chart: {
-      width: 380,
+      width: "100%",
       type: "pie"
     },
-    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-    responsive: [
-      {
-        breakpoint: 480,
-        options: {
-          chart: {
-            width: 200
-          },
-          legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
+    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"]
   };
 
   return (
--- pages/investments.tsx
+++ pages/investments.tsx
@@ -1,7 +1,19 @@
+import styled from 'styled-components';
+
 import { DynamicPieChart } from '../components/apex-charts/dynamics/DynamicPieChart';
 
 const InvestmentsPage = () => {
-  return <DynamicPieChart />;
+  return (
+    <PieChartContainer>
+      <DynamicPieChart
+        series={[86050, 130950, 280000, 827845, 149656, 25500]}
+      />
+    </PieChartContainer>
+  );
 };
 
 export default InvestmentsPage;
+
+const PieChartContainer = styled.div`
+  width: 50%;
+`;
