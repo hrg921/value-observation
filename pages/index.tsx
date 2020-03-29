7664df1e9721fbcd9e0b0964a3350c69d80ebee9
GeonHo Han <hrg921@gmail.com>
2020-03-29 18:08:29 +0900

    feat: add links for subpage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ const IndexPage = () => (
   <div>
     <h1>GeonHo Tony Han Value Observation</h1>
     <h2>Observe all the values around GeonHo Tony Han</h2>
-    <Link href="/incomes">Incomes</Link>
+    <Link href="/incomes">Incomes</Link> <br />
+    <Link href="/investments">Investments</Link> <br />
+    <Link href="/reads">Reads</Link>
   </div>
 );
 
